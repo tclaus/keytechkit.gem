85712e722ebd4d5ae6e68251445825348ba11eb3
Thorsten Claus <thorstenclaus@web.de>
2018-10-24 12:55:23 +0200

    set query with parameters

--- lib/keytechKit/search.rb
+++ lib/keytechKit/search.rb
@@ -13,14 +13,15 @@ module KeytechKit
     end
 
     #options = {};
+    #:q = searchtext - optional parameter: makes a fulltext search, (unusable on very large data)
     #:size
     #:page
     #:attributes = "ALL","None","Lister", "Editor", "GlobalLister", "System", "Secondary", "Explorer"
     #:reload = true
     #:classes = "DEFAULT_DO","DEFAULT_FD", "DEFAULT_MI", "DEFAULT_MAIL","DEFAULT_TASK"
-
-     def query(query,options = {})
-       options[:q] = query
+    #:fields = [<attribute><operator><value>:] - as_do__created_by=jgrant:as_do_changed_by=pmiller - finds all elememnts cerated by jgrant and changed by pmiller
+    # Remeber: dates must be send in epoch format! "/date(1464127200000)/"
+     def query(options = {})
        parameter = {query: options}
        parameter.merge!({ basic_auth: @auth })
        response = self.class.get("/search", parameter)
@@ -28,7 +29,7 @@ module KeytechKit
           self.response = response
           parse_response
         else
-          raise response.response
+          puts "Could not execute query: #{save_response.headers['x-errordescription']}"
        end
      end
 
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -9,25 +9,32 @@ module KeytechKit
 
     it "executes a fulltext query" do
         keytechKit =  Keytech_Kit.new(KeytechKit::DEMO_URL, KeytechKit::DEMO_USER, KeytechKit::DEMO_PASSWORD)
-        elements = keytechKit.search.query("demo")
+        elements = keytechKit.search.query({q: "demo"})
         expect(elements).not_to be nil
         expect(elements.elementList.length).to be > 10
     end
 
     it "executes a fulltext query with pages" do
         keytechKit =  Keytech_Kit.new(KeytechKit::DEMO_URL, KeytechKit::DEMO_USER, KeytechKit::DEMO_PASSWORD)
-        elements = keytechKit.search.query("demo", {page: 1, size: 1})
+        elements = keytechKit.search.query({q:"demo", page: 1, size: 1})
         expect(elements).not_to be nil
         expect(elements.elementList.length).to be == 1
     end
 
     it "executes a fulltext query with group by " do
         keytechKit =  Keytech_Kit.new(KeytechKit::DEMO_URL, KeytechKit::DEMO_USER, KeytechKit::DEMO_PASSWORD)
-        elements = keytechKit.search.query("demo", {groupBy: "Classkey"})
+        elements = keytechKit.search.query({q: "demo", groupBy: "classkey"})
         expect(elements).not_to be nil
 
         expect(elements.groupBy).not_to be nil
         expect(elements.groupBy.values.length).to be > 1
+    end
+
+    it "executes a query with fields (created_by = pmiller) " do
+        keytechKit =  Keytech_Kit.new(KeytechKit::DEMO_URL, KeytechKit::DEMO_USER, KeytechKit::DEMO_PASSWORD)
+        elements = keytechKit.search.query({fields: "created_by=pmiller:changed_by=pmiller", size: 2})
+        expect(elements).not_to be nil
+        expect(elements.elementList.length).to be > 1
     end
 
   end
